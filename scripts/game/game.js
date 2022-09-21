9d1b3f5f77f54ad1f6e46bbcc12d5d4df6fc80e2
Ethan <ethanchang34@yahoo.com>
2022-09-21 17:55:44 -0400

    independent, random block speed

--- scripts/game/game.js
+++ scripts/game/game.js
@@ -119,7 +119,8 @@ class World {
         //randBlockHeight,
         randBlockWidth, // Making the blocks square
         0,
-        (100 / (randBlockWidth * randBlockHeight)) * BLOCKMOVESPEED // 400 is max area of block (20x20)
+        //(100 / (randBlockWidth * randBlockHeight)) * BLOCKMOVESPEED // 400 is max area of block (20x20)
+        (0.5 * Math.random() + 0.5) * BLOCKMOVESPEED
       );
       this.fallingBoxes.add(this.boxList.length);
       this.boxList.push(newFallingBlock);
